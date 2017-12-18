be101f04abda3048b01c39b57e0ad5963bde0f30
JunshengFu <junsheng.fu@yahoo.com>
2017-12-18 21:52:15 +0200

    init repo

---
+++ main.py
@@ -0,0 +1,131 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import tarfile
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from PIL import Image
+from os import path
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+
+def detect_traffic_lights(PATH_TO_TEST_IMAGES_DIR, MODEL_NAME, Num_images, plot_flag=False):
+    """
+    Detect traffic lights and draw bounding boxes around the traffic lights
+    :param PATH_TO_TEST_IMAGES_DIR: testing image directory
+    :param MODEL_NAME: name of the model used in the task
+    :return: boxes: positions of the traffic lights
+    """
+
+    # What model to download
+    MODEL_FILE = MODEL_NAME + '.tar.gz'
+    DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+    # Path to frozen detection graph. This is the actual model that is used for the object detection.
+    PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+    # List of the strings that is used to add correct label for each box.
+    PATH_TO_LABELS = 'mscoco_label_map.pbtxt'
+
+    # number of classes for COCO dataset
+    NUM_CLASSES = 90
+
+
+    #--------Download model----------
+    if path.isdir(MODEL_NAME) is False:
+        opener = urllib.request.URLopener()
+        opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+        tar_file = tarfile.open(MODEL_FILE)
+        for file in tar_file.getmembers():
+          file_name = os.path.basename(file.name)
+          if 'frozen_inference_graph.pb' in file_name:
+            tar_file.extract(file, os.getcwd())
+
+    #--------Load a (frozen) Tensorflow model into memory
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+      od_graph_def = tf.GraphDef()
+      with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+
+    #----------Loading label map
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map,
+                                                                max_num_classes=NUM_CLASSES,
+                                                                use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    def load_image_into_numpy_array(image):
+      (im_width, im_height) = image.size
+      return np.array(image.getdata()).reshape(
+          (im_height, im_width, 3)).astype(np.uint8)
+
+    #--------test images------
+    TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'img_{}.jpg'.format(i)) for i in range(1, Num_images+1) ]
+
+    # Size, in inches, of the output images.
+    IMAGE_SIZE = (12, 8)
+
+    with detection_graph.as_default():
+      with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+        idx = 0
+        for image_path in TEST_IMAGE_PATHS:
+          image = Image.open(image_path)
+          idx += 1
+          # the array based representation of the image will be used later in order to prepare the
+          # result image with boxes and labels on it.
+          image_np = load_image_into_numpy_array(image)
+          # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+          image_np_expanded = np.expand_dims(image_np, axis=0)
+          # Actual detection.
+          (boxes, scores, classes, num) = sess.run(
+              [detection_boxes, detection_scores, detection_classes, num_detections],
+              feed_dict={image_tensor: image_np_expanded})
+
+          # Visualization of the results of a detection.
+          if plot_flag:
+              vis_util.visualize_boxes_and_labels_on_image_array(
+                  image_np,
+                  np.squeeze(boxes),
+                  np.squeeze(classes).astype(np.int32),
+                  np.squeeze(scores),
+                  category_index,
+                  min_score_thresh=.5,
+                  use_normalized_coordinates=True,
+                  line_thickness=3)
+              plt.figure(figsize=IMAGE_SIZE)
+              plt.imshow(image_np)
+
+              # save augmented images into hard drive
+              plt.savefig( 'output_images/ouput_' + str(idx) +'.png')
+              # plt.show()
+
+
+
+if __name__ == "__main__":
+
+    Num_images = 17
+    PATH_TO_TEST_IMAGES_DIR = '/data/udacity/traffic-lights-detector/test_images'
+    MODEL_NAME = 'faster_rcnn_resnet101_coco_11_06_2017'
+
+    detect_traffic_lights(PATH_TO_TEST_IMAGES_DIR, MODEL_NAME, Num_images, plot_flag=True)
